7d82bace1afb723433a12d6c852598f86b99ea42
twentyfourtwo <saalphilipp96@gmail.com>
2020-11-16 21:11:26 +0100

    Add content to "About" Add dynamic html titles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'generalpages/home'
   get 'generalpages/about'
   get 'generalpages/legal'
+
+  root 'generalpages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
