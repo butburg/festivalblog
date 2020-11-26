c57334b66cd19e73853d7ccc6cd6f0f5bbc56c6a
twentyfourtwo <saalphilipp96@gmail.com>
2020-11-26 15:12:37 +0100

    Add footer & header Add rails_default Add basic css Add logo Change root_route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'generalpages/about'
   get 'generalpages/legal'
 
-  root 'generalpages#about'
+  root 'generalpages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
