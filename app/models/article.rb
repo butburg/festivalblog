bd7a6cc9c660948273f62205e9298d8a476c9bdb
Edwin <fire.fm@hotmail.de>
2020-11-26 21:34:53 +0100

    Description maximum set to 120 new title Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5 }
-  validates :title, presence: false
+  validates :title, presence: false, length: { maximum: 120}
   validates :text, presence: true
 end
