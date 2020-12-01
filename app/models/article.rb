ca9936002159f1b3943b8522a8a2b32e1b7c236f
twentyfourtwo <saalphilipp96@gmail.com>
2020-12-01 15:25:27 +0100

    change validating for title, description and text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5 }
-  validates :title, presence: false, length: { maximum: 120}
-  validates :text, presence: true
+  validates :title, presence: true, length: { minimum: 5, maximum: 80 }
+  validates :description, presence: false, length: { minimum: 5, maximum: 120 }
+  validates :text, presence: true, length: { minimum: 20, maximum: 7000 }
 end
