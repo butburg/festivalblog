9046cd5a64cd40ffc6e34dc81fc80f42067f47ed
Edwin <fire.fm@hotmail.de>
2020-12-03 12:10:04 +0100

    better tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,53 @@
-require 'test_helper'
+require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+  #### ROOT HOME ####
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should render Home root" do
+    get root_url
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+
+  #### INDEX SHOW ####
+  test "should get index" do
+    get index
+    assert_response :success
+  end
+
+  test "should render index" do
+    get index
+    assert_select "title", " | #{@base_title}"
+  end
+
+
+  #### NEW ####
+  test "should get new" do
+    get new
+    assert_response :success
+  end
+
+  test "should render new" do
+    get new
+    assert_select "new", " | #{@base_title}"
+  end
+
+
+  #### EDIT ####
+  test "should get edit" do
+    get edit
+    assert_response :success
+  end
+
+  test "should render edit" do
+    get edit
+    assert_select "title", " | #{@base_title}"
+  end
+
 end
--- test/controllers/generalpages_controller_test.rb
+++ test/controllers/generalpages_controller_test.rb
@@ -1,13 +1,9 @@
-require 'test_helper'
+# frozen_string_literal: true
+require "test_helper"
 
 class GeneralpagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Festivalblog"
-  end
-
-  test "should get home" do
-    get generalpages_home_url
-    assert_response :success
   end
 
   test "should get home via root" do
@@ -16,27 +12,27 @@ class GeneralpagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get about" do
-    get generalpages_about_url
+    get about_url
     assert_response :success
   end
 
   test "should get legal" do
-    get generalpages_legal_url
+    get legal_url
     assert_response :success
   end
 
   test "should render home" do
-    get generalpages_home_url
+    get root_url
     assert_select "title", "Home | #{@base_title}"
   end
 
   test "should render about" do
-    get generalpages_about_url
+    get about_url
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should render legal" do
-    get generalpages_legal_url
+    get legal_url
     assert_select "title", "Legal | #{@base_title}"
   end
 
