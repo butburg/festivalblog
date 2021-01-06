f60514f2ec997f2f08cfd429b2d0c0697aed06c6
philipp <philipp.saal@twentyfourtwo.de>
2021-01-06 20:25:18 +0100

    little css changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_120303) do
+ActiveRecord::Schema.define(version: 2020_11_26_143010) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,5 @@ ActiveRecord::Schema.define(version: 2020_12_03_120303) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 
 end
