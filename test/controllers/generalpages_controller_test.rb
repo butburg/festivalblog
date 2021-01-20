a0c2a3dee1a4d51c7470ec21a52a9f949b86647d
Edwin <fire.fm@hotmail.de>
2021-01-20 13:21:06 +0100

    test full_title method separately

--- test/controllers/generalpages_controller_test.rb
+++ test/controllers/generalpages_controller_test.rb
@@ -2,9 +2,6 @@
 require "test_helper"
 
 class GeneralpagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = "Festivalblog"
-  end
 
   test "should get home via root" do
     get root_url
@@ -23,17 +20,17 @@ class GeneralpagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should render home" do
     get root_url
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", full_title("Home")
   end
 
   test "should render about" do
     get about_url
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", full_title("About")
   end
 
   test "should render legal" do
     get legal_url
-    assert_select "title", "Legal | #{@base_title}"
+    assert_select "title", full_title("Legal")
   end
 
 end
